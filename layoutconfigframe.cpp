68b98f501a493e87d705d4fc0e9faea25d8324aa
andryblack <blackicebox@gmail.com>
2010-03-19 14:39:32 +0000

    added losted LayoutConfigFrame added ignoring ui_*.h

---
+++ layoutconfigframe.cpp
@@ -0,0 +1,70 @@
+#include "layoutconfigframe.h"
+#include "ui_layoutconfigframe.h"
+#include "layoutconfig.h"
+
+LayoutConfigFrame::LayoutConfigFrame(QWidget *parent) :
+    QFrame(parent),
+    ui(new Ui::LayoutConfigFrame)
+{
+    ui->setupUi(this);
+    m_config = 0;
+}
+
+LayoutConfigFrame::~LayoutConfigFrame()
+{
+    delete ui;
+}
+
+void LayoutConfigFrame::setConfig(LayoutConfig* config) {
+    m_config = config;
+    if (config) {
+        ui->checkBoxOnePixelOffset->setChecked(config->onePixelOffset());
+        ui->checkBoxPOT->setChecked(config->potImage());
+        ui->spinBoxLeftOffset->setValue(config->offsetLeft());
+        ui->spinBoxTopOffset->setValue(config->offsetTop());
+        ui->spinBoxRightOffset->setValue(config->offsetRight());
+        ui->spinBoxBottomOffset->setValue(config->offsetBottom());
+    }
+}
+
+void LayoutConfigFrame::changeEvent(QEvent *e)
+{
+    QFrame::changeEvent(e);
+    switch (e->type()) {
+    case QEvent::LanguageChange:
+        ui->retranslateUi(this);
+        break;
+    default:
+        break;
+    }
+}
+
+void LayoutConfigFrame::on_checkBoxOnePixelOffset_toggled(bool checked)
+{
+    if (m_config) m_config->setOnePixelOffset(checked);
+}
+
+void LayoutConfigFrame::on_checkBoxPOT_toggled(bool checked)
+{
+    if (m_config) m_config->setPotImage(checked);
+}
+
+void LayoutConfigFrame::on_spinBoxTopOffset_valueChanged(int value)
+{
+    if (m_config) m_config->setOffsetTop(value);
+}
+
+void LayoutConfigFrame::on_spinBoxLeftOffset_valueChanged(int value)
+{
+    if (m_config) m_config->setOffsetLeft(value);
+}
+
+void LayoutConfigFrame::on_spinBoxBottomOffset_valueChanged(int value)
+{
+    if (m_config) m_config->setOffsetBottom(value);
+}
+
+void LayoutConfigFrame::on_spinBoxRightOffset_valueChanged(int value)
+{
+    if (m_config) m_config->setOffsetRight(value);
+}
---
+++ layoutconfigframe.h
@@ -0,0 +1,35 @@
+#ifndef LAYOUTCONFIGFRAME_H
+#define LAYOUTCONFIGFRAME_H
+
+#include <QFrame>
+
+namespace Ui {
+    class LayoutConfigFrame;
+}
+
+class LayoutConfig;
+
+class LayoutConfigFrame : public QFrame {
+    Q_OBJECT
+public:
+    LayoutConfigFrame(QWidget *parent = 0);
+    ~LayoutConfigFrame();
+
+    void setConfig(LayoutConfig* config);
+protected:
+    void changeEvent(QEvent *e);
+
+private:
+    Ui::LayoutConfigFrame *ui;
+    LayoutConfig*   m_config;
+
+private slots:
+    void on_spinBoxRightOffset_valueChanged(int );
+    void on_spinBoxBottomOffset_valueChanged(int );
+    void on_spinBoxLeftOffset_valueChanged(int );
+    void on_spinBoxTopOffset_valueChanged(int );
+    void on_checkBoxPOT_toggled(bool checked);
+    void on_checkBoxOnePixelOffset_toggled(bool checked);
+};
+
+#endif // LAYOUTCONFIGFRAME_H
